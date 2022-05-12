1db35bf3e9528032b9e55806ead798dbeef12820
react-shopping-cart <tattuong17@gmail.com>
2022-05-12 09:54:15 +0700

    add

--- js/js.js
+++ js/js.js
@@ -5,6 +5,7 @@ const editModalForm = document.querySelector("#editModal .form-user");
 const deleteModalForm = document.querySelector("#deleteModal .form-user");
 const addModalForm = document.querySelector("#addModal .form-user");
 
+
 let id = "";
 let listUsers = [];
 let listSelectedUser = [];
@@ -38,63 +39,117 @@ function deleteAllUsers() {
       .then(() => fetchUsers());
   });
 }
-
 function fetchUsers() {
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      listUsers = data.slice();
-      renderUsers();
-    });
-}
-//render listUser
-
-function renderUsers() {
-  tableUsers.innerHTML = "";
-  listUsers.map((user) => {
-    const output = ` 
-  <tr data-id= '${user.id}'>
-      <td>
-        <span class="custom-checkbox">
-            <input onchange="addListSelected(${user.id})" type="checkbox" id="checkbox1${user.id}" name="options[]" value="1">
-          <label for="checkbox1"></label>
-        </span> 
-      </td>
-      <td>${user.name}</td>
-      <td>${user.email}</td>  
-      <td>${user.address}</td>
-      <td>${user.phone}</td>
-      <td>
-        <a href="#editModal" class="btn-edit" ><i class="bi bi-pencil-fill" data-toggle="modal" title="Edit" style="font-size: 20px; color:#FFC107;"></i></a>
-        <a href="#deleteModal" class="btn-del"><i class="bi bi-trash-fill" data-toggle="tooltip" title="Delete" style="font-size: 20px; color:red"	></i></a>
-      </td>
-    </tr>       
-`;
-    tableUsers.insertAdjacentHTML("beforeend", output);
-
-    const btnDel = document.querySelector(`[data-id = '${user.id}'] .btn-del`);
-    btnDel.addEventListener("click", (e) => {
-      e.preventDefault();
-      id = user.id;
-      $("#deleteModal").modal("show");
-    });
-    const btnEdit = document.querySelector(
-      `[data-id = '${user.id}'] .btn-edit`
-    );
-    btnEdit.addEventListener("click", (e) => {
-      e.preventDefault();
-      id = user.id;
-      $("#editModal").modal("show");
-      (editModalForm.name.value = user.name),
-        (editModalForm.email.value = user.email),
-        (editModalForm.address.value = user.address),
-        (editModalForm.phone.value = user.phone);
-    });
+  $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: {
+      data: "testdata",
+    },
+    success: function (data) {
+      console.log(data);
+      $.each(data, function(key, user) {
+        $("#table-user").append(`<tr data-id= '${user.id}'>
+        <td>
+          
+          <span class="custom-checkbox">
+              <input onchange="addListSelected(${user.id})" type="checkbox" id="checkbox1${user.id}" name="options[]" value="1">
+            <label for="checkbox1"></label>
+          </span> 
+        </td>
+        <td>${user.name}</td>
+        <td>${user.email}</td>  
+        <td>${user.address}</td>
+        <td>${user.phone}</td>
+        <td>
+          <a href="#editModal" class="btn-edit" ><i class="bi bi-pencil-fill" data-toggle="modal" title="Edit" style="font-size: 20px; color:#FFC107;"></i></a>
+          <a href="#deleteModal" class="btn-del"><i class="bi bi-trash-fill" data-toggle="tooltip" title="Delete" style="font-size: 20px; color:red"	></i></a>
+        </td>
+      </tr>`);
+        const btnDel = document.querySelector(
+          `[data-id = '${user.id}'] .btn-del`
+        );
+        btnDel.addEventListener("click", (e) => {
+          e.preventDefault();
+          id = user.id;
+          $("#deleteModal").modal("show");
+        });
+        const btnEdit = document.querySelector(
+          `[data-id = '${user.id}'] .btn-edit`
+        );
+        btnEdit.addEventListener("click", (e) => {
+          e.preventDefault();
+          id = user.id;
+          $("#editModal").modal("show");
+          (editModalForm.name.value = user.name),
+            (editModalForm.email.value = user.email),
+            (editModalForm.address.value = user.address),
+            (editModalForm.phone.value = user.phone);
+        });
+        console.log(key);
+      });
+      
+    },
   });
 }
+// function fetchUsers() {
+//   fetch(url)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       listUsers = data.slice();
+//       renderUsers();
+//     });
+// }
+// //render listUser
+
+// function renderUsers() {
+//   tableUsers.innerHTML = "";
+//   listUsers.map((user) => {
+//     const output = ` 
+//   <tr data-id= '${user.id}'>
+//       <td>
+//         <span class="custom-checkbox">
+//             <input onchange="addListSelected(${user.id})" type="checkbox" id="checkbox1${user.id}" name="options[]" value="1">
+//           <label for="checkbox1"></label>
+//         </span> 
+//       </td>
+//       <td>${user.name}</td>
+//       <td>${user.email}</td>  
+//       <td>${user.address}</td>
+//       <td>${user.phone}</td>
+//       <td>
+//         <a href="#editModal" class="btn-edit" ><i class="bi bi-pencil-fill" data-toggle="modal" title="Edit" style="font-size: 20px; color:#FFC107;"></i></a>
+//         <a href="#deleteModal" class="btn-del"><i class="bi bi-trash-fill" data-toggle="tooltip" title="Delete" style="font-size: 20px; color:red"	></i></a>
+//       </td>
+//     </tr>       
+// `;
+//     tableUsers.insertAdjacentHTML("beforeend", output);
+
+//     const btnDel = document.querySelector(`[data-id = '${user.id}'] .btn-del`);
+//     btnDel.addEventListener("click", (e) => {
+//       e.preventDefault();
+//       id = user.id;
+//       $("#deleteModal").modal("show");
+//     });
+//     const btnEdit = document.querySelector(
+//       `[data-id = '${user.id}'] .btn-edit`
+//     );
+//     btnEdit.addEventListener("click", (e) => {
+//       e.preventDefault();
+//       id = user.id;
+//       $("#editModal").modal("show");
+//       (editModalForm.name.value = user.name),
+//         (editModalForm.email.value = user.email),
+//         (editModalForm.address.value = user.address),
+//         (editModalForm.phone.value = user.phone);
+//     });
+//   });
+// }
 
 //del uniqueUser
 function handleDeleteUser() {
+ 
   deleteModalForm.addEventListener("click", (e) => {
     e.preventDefault();
     fetch(`${url}/${id}`, {
@@ -108,7 +163,7 @@ function handleDeleteUser() {
 //addUser
 function handleAddUser() {
   console.log("sd");
-  addModalForm.addEventListener("submit", (e) => {
+  addModalForm.addEventListener("click", (e) => {
     e.preventDefault();
     fetch(url, {
       method: "POST",
