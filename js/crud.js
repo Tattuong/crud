77d7e4c2da1fd8ea910bd34bcddb4bd182a712c4
react-shopping-cart <tattuong17@gmail.com>
2022-05-17 11:25:16 +0700

    jquery

--- js/crud.js
+++ js/crud.js
@@ -1,1 +1,30 @@
-        
+// Render employs
+
+function renderEmployes (){     
+    
+}
+//resetInput
+
+function resetEmployes  () {
+
+}
+
+// handelDeleleEmployes
+
+function handelDeleleEmployes() {
+
+}
+
+//handelAddEmployes
+
+function handelAddEmployes (){    
+}
+
+//handelAddEmployes
+function handelAddEmployes (){
+
+}
+function main () {
+    
+}
+
--- js/js.js
+++ js/js.js
@@ -14,84 +14,28 @@ function checkAllUsers() {
   console.log(listUsers);
   const checkboxValue = document.getElementById("selectedAll");
   if (checkboxValue) {
-    listSelectedUser = listUsers.map();
+     listSelectedUser = listUsers.map();
   }
 }
 
 //addIdUser
 function addListSelected(value) {
   const checkboxValue = document.getElementById(`checkbox1${value}`).checked;
-  if (checkboxValue) {
-    listSelectedUser = listSelectedUser.concat(value);
-    console.log("btn_", value);
-  } else {
-    listSelectedUser = listSelectedUser.filter((user) => user !== value);
+
   }
+  
+function handelAddCustomer() {
+  
+
 }
 
-//del ClickCheckbox
-function deleteAllUsers() {
-  listSelectedUser.map((id) => {
-    fetch(`${url}/${id}`, {
-      method: "DELETE",
-    })
-      .then((res) => res.json())
-      .then(() => fetchUsers());
-  });
+function handleDeleteCustomer() {
+
 }
-// function fetchUsers() {
-//   $.ajax({
-//     url: url,
-//     method: "GET",
-//     dataType: "json",
-//     data: {
-//       data: "testdata",
-//     },
-//     success: function (data) {
-//       console.log(data);
-//       $.each(data, function(key, user) {
-//         $("#table-user").append(`<tr data-id= '${user.id}'>
-//         <td>
-//           <span class="custom-checkbox">
-//               <input onchange="addListSelected(${user.id})" type="checkbox" id="checkbox1${user.id}" name="options[]" value="1">
-//             <label for="checkbox1"></label>
-//           </span> 
-//         </td>
-//         <td>${user.name}</td>
-//         <td>${user.email}</td>  
-//         <td>${user.address}</td>
-//         <td>${user.phone}</td>
-//         <td>
-//           <a href="#editModal" class="btn-edit" ><i class="bi bi-pencil-fill" data-toggle="modal" title="Edit" style="font-size: 20px; color:#FFC107;"></i></a>
-//           <a href="#deleteModal" class="btn-del"><i class="bi bi-trash-fill" data-toggle="tooltip" title="Delete" style="font-size: 20px; color:red"	></i></a>
-//         </td>
-//       </tr>`);
-//         const btnDel = document.querySelector(
-//           `[data-id = '${user.id}'] .btn-del`
-//         );
-//         btnDel.addEventListener("click", (e) => {
-//           e.preventDefault();
-//           id = user.id;
-//           $("#deleteModal").modal("show");
-//         });
-//         const btnEdit = document.querySelector(
-//           `[data-id = '${user.id}'] .btn-edit`
-//         );
-//         btnEdit.addEventListener("click", (e) => {
-//           e.preventDefault();
-//           id = user.id;
-//           $("#editModal").modal("show");
-//           (editModalForm.name.value = user.name),
-//             (editModalForm.email.value = user.email),
-//             (editModalForm.address.value = user.address),
-//             (editModalForm.phone.value = user.phone);
-//         });
-//         console.log(key);
-//       });
-      
-//     },
-//   });
-// }
+
+
+
+
 function fetchUsers() {
   fetch(url)
     .then((res) => res.json())
@@ -100,8 +44,6 @@ function fetchUsers() {
       renderUsers();
     });
 }
-//render listUser
-
 function renderUsers() {
   tableUsers.innerHTML = "";
   listUsers.map((user) => {
@@ -123,6 +65,8 @@ function renderUsers() {
       </td>
     </tr>       
 `;
+
+
     tableUsers.insertAdjacentHTML("beforeend", output);
 
     const btnDel = document.querySelector(`[data-id = '${user.id}'] .btn-del`);
@@ -146,67 +90,139 @@ function renderUsers() {
   });
 }
 
-//del uniqueUser
-  function handleDeleteUser() {
- 
-  deleteModalForm.addEventListener("click", (e) => {
-    e.preventDefault();
+
+
+// ClickCheckbox
+function deleteUsers() {
+  listSelectedUser.map((id) => {
     fetch(`${url}/${id}`, {
       method: "DELETE",
     })
       .then((res) => res.json())
-      .then(() => fetchUsers());
+
   });
 }
 
+
+
+// function fetchUsers() {
+//   $.ajax({
+//     url: url,
+//     method: "GET",
+//     dataType: "json",
+//     data: {
+//       data: "testdata",
+//     },
+//     success: function (data) {
+//       console.log(data);
+//       $.each(data, function(key, user) {
+//         $("#table-user").append(`<tr data-id= '${user.id}'>
+//         <td>
+//           <span class="custom-checkbox">
+//               <input onchange="addListSelected(${user.id})" type="checkbox" id="checkbox1${user.id}" name="options[]" value="1">
+//             <label for="checkbox1"></label>
+//           </span>
+//         </td>
+//         <td>${user.name}</td>
+//         <td>${user.email}</td>
+//         <td>${user.address}</td>
+//         <td>${user.phone}</td>
+//         <td>
+//           <a href="#editModal" class="btn-edit" ><i class="bi bi-pencil-fill" data-toggle="modal" title="Edit" style="font-size: 20px; color:#FFC107;"></i></a>
+//           <a href="#deleteModal" class="btn-del"><i class="bi bi-trash-fill" data-toggle="tooltip" title="Delete" style="font-size: 20px; color:red"	></i></a>
+//         </td>
+//       </tr>`);
+//         const btnDel = document.querySelector(
+//           `[data-id = '${user.id}'] .btn-del`
+//         );
+//         btnDel.addEventListener("click", (e) => {
+//           e.preventDefault();
+//           id = user.id;
+//           $("#deleteModal").modal("show");
+//         });
+//         const btnEdit = document.querySelector(
+//           `[data-id = '${user.id}'] .btn-edit`
+//         );
+
+//         btnEdit.addEventListener("click", (e) => {
+//           e.preventDefault();
+//           id = user.id;
+//           $("#editModal").modal("show");
+//           (editModalForm.name.value = user.name),
+//             (editModalForm.email.value = user.email),
+//             (editModalForm.address.value = user.address),
+//             (editModalForm.phone.value = user.phone);
+//         });
+//         console.log(key);
+//       });
+
+//     },
+//   });
+// }
+
+//render listUser
+
+//del uniqueUser
+// function handleDeleteUser() {
+//   deleteModalForm.addEventListener("click", () => {
+   
+//     fetch(`${url}/${id}`, {
+//       method: "DELETE",
+//     })
+//       .then((res) => res.json())
+//       .then(() => fetchUsers());
+//   });
+// }
 //addUser
-function handleAddUser() {
-  console.log("sd");
-  addModalForm.addEventListener("click", (e) => {
-    e.preventDefault();
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: addModalForm.name.value,
-        email: addModalForm.email.value,
-        address: addModalForm.address.value,
-        phone: addModalForm.phone.value,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        const dataArr = [];
-        dataArr.push(data);
-        fetchUsers(dataArr);
-      });
-  });
-}
+// function handleAddUser() {
+//   console.log("sd");
+//   addModalForm.addEventListener("click", () => {
+
+//     fetch(url, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         name: addModalForm.name.value,
+//         email: addModalForm.email.value,
+//         address: addModalForm.address.value,
+//         phone: addModalForm.phone.value,
+//       }),
+//     })
+//       .then((res) => res.json())
+//       .then((data) => {
+//         const dataArr = [];
+//         dataArr.push(data);
+//         fetchUsers(dataArr);
+//       });
+//   });
+// }
 
 //edit User
-function handleEditUser() {
-  editModalForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    fetch(`${url}/${id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
+// function handleEditUser() {
+//   editModalForm.addEventListener("click", () => {
 
-      body: JSON.stringify({
-        name: editModalForm.name.value,
-        email: editModalForm.email.value,
-        address: editModalForm.address.value,
-        phone: editModalForm.phone.value,
-      }),
-    })
-      .then((res) => res.json())    
-      .then((res) => res.fetchUsers())
-  });
-}
+//       fetch(`${url}/${id}`, {
+//       method: "PATCH",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+
+//       body: JSON.stringify({
+//         name: editModalForm.name.value,
+//         email: editModalForm.email.value,
+//         address: editModalForm.address.value,
+//         phone: editModalForm.phone.value,
+//       }),
+//     })
+//       .then((res) => res.json())
+//       .then((res) => res.fetchUsers());
+//   });
+//
+
 function start() {
   fetchUsers();
 }
+
 start();
